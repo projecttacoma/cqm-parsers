be4e0ead9f079bac2ddcdf898ddae173e025800a
Czulada, David S <dczulada@mitre.org>
2018-06-22 15:39:02 -0400

    Support RelatedTo

--- lib/qrda-import/base-importers/section_importer.rb
+++ lib/qrda-import/base-importers/section_importer.rb
@@ -6,10 +6,7 @@ module QRDA
       def initialize(entry_finder)
         @entry_finder = entry_finder
         @code_xpath = "./cda:code"
-        @id_xpath = "./cda:id"
-        #@status_xpath = nil
-        #@priority_xpath = nil
-        #@description_xpath = "./cda:code/cda:originalText/cda:reference[@value] | ./cda:text/cda:reference[@value]"
+        @entry_id_map = {}
         @check_for_usable = true
         @entry_class = QDM::DataElement
       end
@@ -31,7 +28,7 @@ module QRDA
             entry_list << entry
           end
         end
-        entry_list
+        return entry_list, @entry_id_map
       end
 
       def usable_entry?(entry)
@@ -40,57 +37,26 @@ module QRDA
 
       def create_entry(entry_element, nrh = NarrativeReferenceHandler.new)
         entry = @entry_class.new
-        #extract_id(entry_element, entry)
+        @entry_id_map[extract_id(entry_element, "./cda:id")] = entry.id
         entry.dataElementCodes = extract_codes(entry_element, @code_xpath)
         extract_dates(entry_element, entry)
         if @result_xpath
           entry.result = extract_result_values(entry_element)
         end
         extract_reason_or_negation(entry_element, entry)
-        # extract_description(entry_element, entry, nrh)
-        # if @status_xpath
-        #   extract_status(entry_element, entry)
-        # end
         entry
       end
 
       private
 
-      def extract_description(parent_element, entry, nrh)
-        # orig_text_ref_element = parent_element.at_xpath(@description_xpath)
-        # desc_ref_element = parent_element.at_xpath("./cda:text/cda:reference")
-        # if orig_text_ref_element && orig_text_ref_element['value']
-        #   entry.description = nrh.lookup_tag(orig_text_ref_element['value'])
-        # elsif desc_ref_element && desc_ref_element['value']
-        #   entry.description = nrh.lookup_tag(desc_ref_element['value'])
-        # else
-        #   entry.description = parent_element.at_xpath("./cda:text").try("text")
-        # end
-      end
-
-      def extract_status(parent_element, entry)
-        # status_element = parent_element.at_xpath(@status_xpath)
-        # if status_element
-        #   entry.status_code = {HQMF::Util::CodeSystemHelper.code_system_for(status_element['codeSystem']) => [status_element['code']]}
-        # end
-      end
-
-      def extract_id(parent_element, entry)
-        # id_element = parent_element.at_xpath(@id_xpath)
-        # if id_element
-        #   identifier = CDAIdentifier.new
-        #   identifier.root = id_element['root']
-        #   identifier.extension = id_element['extension']
-        #   entry['cda_identifier'] = identifier
-        # end
-      end
-
-      def extract_reason_description(parent_element, entry, nrh)
-        # code_elements = parent_element.xpath(@description_xpath)
-        # code_elements.each do |code_element|
-        #   tag = code_element['value']
-        #   entry.description = nrh.lookup_tag(tag)
-        # end
+      def extract_id(parent_element, id_xpath)
+        id_element = parent_element.at_xpath(id_xpath)
+        if id_element
+          identifier = CDAIdentifier.new
+          identifier.root = id_element['root']
+          identifier.extension = id_element['extension']
+          identifier
+        end
       end
 
       def extract_codes(coded_element, code_xpath)
@@ -163,34 +129,8 @@ module QRDA
             return QDM::Quantity.new(value.strip.to_i, value_element['unit'])
           elsif value_element['code'].present?
             return code_if_present(value_element)
-          else
-            # value = value_element.text
-            # unit = value_element['unit']
-            # entry.set_value(value.strip, unit)
           end
         end
-      end
-
-      def import_actor(actor_element)
-        # return ProviderImporter.instance.extract_provider(actor_element)
-      end
-
-      def import_organization(organization_element)
-        # return OrganizationImporter.instance.extract_organization(organization_element)
-      end
-
-      def import_person(person_element)
-        # return unless person_element
-        # person = Person.new
-        # name_element = person_element.at_xpath("./cda:name")
-        # if name_element
-        #   person.title = name_element.at_xpath("./cda:title").try(:text)
-        #   person.given_name = name_element.at_xpath("./cda:given").try(:text)
-        #   person.family_name = name_element.at_xpath("./cda:family").try(:text)
-        # end
-        # person.addresses = person_element.xpath("./cda:addr").map { |addr| import_address(addr) }
-        # person.telecoms = person_element.xpath("./cda:telecom").map { |tele| import_telecom(tele) }
-        # return person
       end
 
       # extracts the reason or negation data. if an element is negated and the code has a null flavor, a random code is assigned for calculation
@@ -220,22 +160,6 @@ module QRDA
             entry.dataElementCodes = [{ code: code_element['sdtc:valueSet'], codeSystem: 'NA_VALUESET' }]
           end
         end
-      end
-
-      def extract_code(parent_element, code_xpath, code_system=nil)
-        # code_element = parent_element.at_xpath(code_xpath)
-        # code_hash = nil
-        # if code_element
-        #   code_hash = {'code' => code_element['code']}
-        #   if code_system
-        #     code_hash['codeSystem'] = code_system
-        #   else
-        #     code_hash['codeSystemOid'] = code_element['codeSystem']
-        #     code_hash['codeSystem'] = HQMF::Util::CodeSystemHelper.code_system_for(code_hash['codeSystemOid'])
-        #   end
-        # end
-
-        # code_hash
       end
 
       def extract_scalar(parent_element, scalar_xpath)
@@ -271,6 +195,15 @@ module QRDA
           facility.locationPeriod = extract_interval(facility_element, './cda:time')
           entry.facilityLocations = [facility]
         end
+      end
+
+      def extract_related_to(parent_element)
+        related_to_elements = parent_element.xpath(@related_to_xpath)
+        related_ids = []
+        related_to_elements.each do |related_to_element|
+          related_ids << extract_id(related_to_element, './sdtc:id')
+        end
+        related_ids
       end
 
     end
