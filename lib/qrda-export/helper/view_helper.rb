47ac9884d50738545e075a92550f04b24c88fcc5
Czulada, David S <dczulada@mitre.org>
2018-08-20 13:48:21 -0400

    initial support for qrda cat III export

---
+++ lib/qrda-export/helper/view_helper.rb
@@ -0,0 +1,27 @@
+module Qrda
+  module Export
+    module Helper
+      module ViewHelper
+        def measures
+          @measures.only(:hqmf_id, :hqmf_set_id, :description).as_json
+        end
+
+        def random_id
+          UUID.generate
+        end
+
+        def as_id
+          self['$oid']
+        end
+
+        def object_id
+          self[:_id]['$oid']
+        end
+
+        def submission_program
+          @submission_program
+        end
+      end
+    end
+  end
+end
