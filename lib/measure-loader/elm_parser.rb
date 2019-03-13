bb30ae992ee8413bb0a2b981ebf46a85d76703ee
Cole Springate-Combs <cspringatecombs@mitre.org>
2019-03-12 21:31:45 -0400

    add tests and tiny refactor

--- lib/measure-loader/elm_parser.rb
+++ lib/measure-loader/elm_parser.rb
@@ -4,20 +4,19 @@ module Measures
     @fields = ['expression', 'operand', 'suchThat']
 
     def self.parse(doc)
-      @doc = doc
-      @localid_to_type_map = generate_localid_to_type_map
+      localid_to_type_map = generate_localid_to_type_map(doc)
       ret = {
         statements: [],
         identifier: {}
       }
       # extract library identifier data
-      ret[:identifier][:id] = @doc.css("identifier").attr("id").value
-      ret[:identifier][:version] = @doc.css("identifier").attr("version").value
+      ret[:identifier][:id] = doc.css("identifier").attr("id").value
+      ret[:identifier][:version] = doc.css("identifier").attr("version").value
 
       # extracts the fields of type "annotation" and their children.
-      annotations = @doc.css("annotation")
+      annotations = doc.css("annotation")
       annotations.each do |node|
-        node, define_name = parse_node(node)
+        node, define_name = parse_node(node, localid_to_type_map)
         unless define_name.nil?
           node[:define_name] = define_name
           ret[:statements] << node
@@ -28,7 +27,7 @@ module Measures
 
     # Recursive function that traverses the annotation tree and constructs a representation
     # that will be compatible with the front end.
-    def self.parse_node(node)
+    def self.parse_node(node, localid_to_type_map)
       ret = {
         children: []
       }
@@ -43,10 +42,10 @@ module Measures
           ret[:children] << clause
           define_name = clause_text.split("\"")[1] if clause_text.strip.starts_with?("define")
         else
-          node_type = @localid_to_type_map[child['r']] unless child['r'].nil?
+          node_type = localid_to_type_map[child['r']] unless child['r'].nil?
           # Parses the current child recursively. child_define_name will bubble up to indicate which
           # statement is currently being traversed.
-          node, child_define_name = parse_node(child)
+          node, child_define_name = parse_node(child, localid_to_type_map)
           node[:node_type] = node_type  unless node_type.nil?
           node[:ref_id] = child['r'] unless child['r'].nil?
           ret[:children] << node
@@ -56,10 +55,10 @@ module Measures
       return ret, define_name
     end
 
-    def self.generate_localid_to_type_map
+    def self.generate_localid_to_type_map(doc)
       localid_to_type_map = {}
       @fields.each do |field|
-        nodes = @doc.css(field + '[localId][xsi|type]')
+        nodes = doc.css(field + '[localId][xsi|type]')
         nodes.each {|node| localid_to_type_map[node['localId']] = node['xsi:type']}
       end
       return localid_to_type_map
