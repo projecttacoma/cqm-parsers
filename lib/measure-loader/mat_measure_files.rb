37817422f30a94c9e5327b9c5f9cf9a802a1306c
Joe Kotanchik <joseph.kotanchik@semanticbits.com>
2020-08-25 17:16:41 -0400

    Removing HQMF parsers and related tests

--- lib/measure-loader/mat_measure_files.rb
+++ lib/measure-loader/mat_measure_files.rb
@@ -2,7 +2,7 @@ require 'zip'
 
 module Measures
   class MATMeasureFiles
-    attr_accessor :hqmf_xml, :cql_libraries, :human_readable, :components
+    attr_accessor :cql_libraries, :human_readable, :components
 
     FHIR_VERSION = '4.0.1'
 
@@ -17,12 +17,10 @@ module Measures
       end
     end
 
-    def initialize(hqmf_xml, cql_libraries, human_readable)
-      @hqmf_xml = hqmf_xml
+    def initialize(cql_libraries, human_readable)
       @cql_libraries = cql_libraries
       @human_readable = human_readable
 
-      raise MeasureLoadingInvalidPackageException.new("Measure package missing required element: HQMF XML File") if @hqmf_xml.nil?
       raise MeasureLoadingInvalidPackageException.new("Measure package missing required element: Human Readable Document") if @human_readable.nil?
       raise MeasureLoadingInvalidPackageException.new("Measure package missing required element: CQL Libraries") if @cql_libraries.nil? || @cql_libraries.empty?
     end
@@ -81,60 +79,6 @@ module Measures
         folders
       rescue StandardError
         raise MeasureLoadingInvalidPackageException.new("The uploaded file is not a zip file.")
-      end
-
-      def make_measure_artifacts(measure_files)
-        library_count = measure_files[:cqls].length
-        unless (measure_files[:elm_xmls].length == library_count && measure_files[:elms].length == library_count)
-          raise MeasureLoadingInvalidPackageException.new("Each library must have a CQL file, an ELM JSON file, and an ELM XML file.")
-        end
-
-        cql_libraries = []
-        library_count.times do |i|
-          elm_annotation = measure_files[:elm_xmls][i]
-          elm = measure_files[:elms][i]
-          cql = measure_files[:cqls][i]
-
-          id, version = get_library_identifier(elm_annotation)
-          verify_library_versions_match(cql, elm, id, version)
-          cql_libraries << LogicLibraryContent.new(id, version, cql, elm, elm_annotation)
-        end
-
-        return new(measure_files[:hqmf_xml], cql_libraries, measure_files[:human_readable])
-      end
-
-      def parse_measure_files(folder)
-        measure_files = { cqls: [], elms: [], elm_xmls: [] }
-        folder[:files].sort_by! { |h| h[:basename] }
-        folder[:files].each do |file|
-          case file[:basename].extname.to_s
-          when '.cql'
-            measure_files[:cqls] << file[:contents]
-          when '.json'
-            begin
-              measure_files[:elms] << JSON.parse(file[:contents], max_nesting: 1000)
-            rescue StandardError
-              raise MeasureLoadingInvalidPackageException.new("Unable to parse json file #{basename}")
-            end
-          when '.html'
-            raise MeasureLoadingInvalidPackageException.new("Multiple HumanReadable docs found in same folder") unless measure_files[:human_readable].nil?
-            measure_files[:human_readable] = file[:contents]
-          when '.xml'
-            begin
-              doc = Nokogiri::XML(file[:contents]) { |config| config.noblanks }
-            rescue StandardError
-              raise MeasureLoadingInvalidPackageException.new("Unable to parse xml file #{basename}")
-            end
-            if doc.root.name == 'QualityMeasureDocument'
-              raise MeasureLoadingInvalidPackageException.new("Multiple QualityMeasureDocuments found in same folder") unless measure_files[:hqmf_xml].nil?
-              measure_files[:hqmf_xml] = doc
-            elsif doc.root.name == 'library'
-              measure_files[:elm_xmls] << doc
-            end
-          end
-        end
-        raise MeasureLoadingInvalidPackageException.new("Measure folder found with no hqmf") if measure_files[:hqmf_xml].nil?
-        return measure_files
       end
 
       def get_library_identifier(elm_annotation)
