b890b31066f6e6a657c35d610497e2ddf5e75a82
Joe Kotanchik <56264529+jkotanchik-SB@users.noreply.github.com>
2020-09-14 15:39:11 -0400

    [MAT-1757] Replacing VSAC Username/Password with API Key in VSAC Calls (#114)
    * Replacing vsac username/password with api key.

--- test/unit/vsac/vsac_api_test.rb
+++ test/unit/vsac/vsac_api_test.rb
@@ -18,7 +18,7 @@ class VSACApiTest < Minitest::Test
 
   def test_get_valueset_with_version
     VCR.use_cassette("vsac_vs_version") do
-      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], username: ENV['VSAC_USERNAME'], password: ENV['VSAC_PASSWORD'])
+      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], api_key: ENV['VSAC_API_KEY'])
       vs = api.get_valueset("2.16.840.1.113883.3.600.1.1834", version: "MU2 EP Update 2014-07-01")
       assert vs
       doc = Nokogiri::XML(vs)
@@ -28,7 +28,7 @@ class VSACApiTest < Minitest::Test
 
   def test_get_valuest_with_include_draft_no_profile
     VCR.use_cassette("vsac_vs_include_draft_no_profile") do
-      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], username: ENV['VSAC_USERNAME'], password: ENV['VSAC_PASSWORD'])
+      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], api_key: ENV['VSAC_API_KEY'])
       assert_raises Util::VSAC::VSACArgumentError do
         api.get_valueset("2.16.840.1.113883.3.600.1.1834", include_draft: true)
       end
@@ -37,7 +37,7 @@ class VSACApiTest < Minitest::Test
 
   def test_get_valueset_with_include_draft_specified_profile
     VCR.use_cassette("vsac_vs_include_draft_with_profile") do
-      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], username: ENV['VSAC_USERNAME'], password: ENV['VSAC_PASSWORD'])
+      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], api_key: ENV['VSAC_API_KEY'])
       vs = api.get_valueset("2.16.840.1.113762.1.4.1", include_draft: true, profile: "eCQM Update 2018-05-04")
       assert vs
       doc = Nokogiri::XML(vs)
@@ -47,7 +47,7 @@ class VSACApiTest < Minitest::Test
 
   def test_get_valueset_no_options
     VCR.use_cassette("vsac_vs_no_options") do
-      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], username: ENV['VSAC_USERNAME'], password: ENV['VSAC_PASSWORD'])
+      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], api_key: ENV['VSAC_API_KEY'])
       vs = api.get_valueset("2.16.840.1.113762.1.4.1")
       assert vs
       doc = Nokogiri::XML(vs)
@@ -57,7 +57,7 @@ class VSACApiTest < Minitest::Test
 
   def test_get_valueset_not_found
     VCR.use_cassette("vsac_vs_not_found") do
-      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], username: ENV['VSAC_USERNAME'], password: ENV['VSAC_PASSWORD'])
+      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], api_key: ENV['VSAC_API_KEY'])
       assert_raises Util::VSAC::VSNotFoundError do
         api.get_valueset("bad.oid")
       end
@@ -66,7 +66,7 @@ class VSACApiTest < Minitest::Test
 
   def test_get_valueset_with_release
     VCR.use_cassette("vsac_vs_release") do
-      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], username: ENV['VSAC_USERNAME'], password: ENV['VSAC_PASSWORD'])
+      api = Util::VSAC::VSACAPI.new(config: APP_CONFIG['vsac'], api_key: ENV['VSAC_API_KEY'])
       vs = api.get_valueset("2.16.840.1.113762.1.4.1", release: "MU2 EP Update 2014-05-30")
       assert vs
       doc = Nokogiri::XML(vs)
