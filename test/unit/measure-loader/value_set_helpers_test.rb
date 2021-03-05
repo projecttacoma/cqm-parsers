8e65f42de368d27d72d39b68118e722c4b10a3ff
Serhii Ilin <serhii.ilin@semanticbits.com>
2021-03-05 11:12:40 -0500

    MAT-2745 Update Codes/Codesystems to Pull from JSON FIle

--- test/unit/measure-loader/value_set_helpers_test.rb
+++ test/unit/measure-loader/value_set_helpers_test.rb
@@ -10,4 +10,11 @@ class ValueSetHelpersTest < Minitest::Test
     assert_equal expected_hash, orig_hash
   end
 
+  def test_code_systems_by_name
+    code_systems = Measures::ValueSetHelpers.code_systems_by_name()
+    assert code_systems != nil
+    assert_equal 84, code_systems.keys.size
+    assert_equal 84, code_systems.values.size
+  end
+
 end
