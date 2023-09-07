646f119e36a6c7c09a4f5e1f7990ba4605e6b852
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2023-09-07 11:13:11 -0400

    Replace TGT/ST authn flow with Basic Authn containing the umls api key.

--- test/unit/measure-loader/cql_loader_composite_test.rb
+++ test/unit/measure-loader/cql_loader_composite_test.rb
@@ -15,7 +15,7 @@ class CQLLoaderTest < Minitest::Test
   def test_invalid_composite_measure_with_component_measure_missing_xml_file
     VCR.use_cassette('measure__test_invalid_composite_measure_with_component_measure_missing_xml_file', @vcr_options) do
       measure_file = File.new File.join(@fixtures_path, 'CMSAWA_v5_6_Artifacts_missing_file.zip')
-      value_set_loader = Measures::VSACValueSetLoader.new(options: @vsac_options, ticket_granting_ticket: get_ticket_granting_ticket_using_env_vars)
+      value_set_loader = Measures::VSACValueSetLoader.new(options: @vsac_options, vsac_api_key: test_api_key)
       loader = Measures::CqlLoader.new(measure_file, @measure_details, value_set_loader)
       assert_raises Measures::MeasureLoadingInvalidPackageException do
         loader.extract_measures
@@ -26,7 +26,7 @@ class CQLLoaderTest < Minitest::Test
   def test_invalid_composite_measure_with_missing_component_measure
     VCR.use_cassette('measure__invalid_composite_measure_with_missing_component_measure', @vcr_options) do
       measure_file = File.new File.join(@fixtures_path, 'CMSAWA_v5_6_Artifacts_missing_component.zip')
-      value_set_loader = Measures::VSACValueSetLoader.new(options: @vsac_options, ticket_granting_ticket: get_ticket_granting_ticket_using_env_vars)
+      value_set_loader = Measures::VSACValueSetLoader.new(options: @vsac_options, vsac_api_key: test_api_key)
       loader = Measures::CqlLoader.new(measure_file, @measure_details, value_set_loader)
       assert_raises Measures::MeasureLoadingInvalidPackageException do
         loader.extract_measures
@@ -37,7 +37,7 @@ class CQLLoaderTest < Minitest::Test
   def test_invalid_composite_measure_with_missing_composite_measure_files
     VCR.use_cassette('measure__invalid_composite_measure_with_missing_composite_measure_files', @vcr_options) do
       measure_file = File.new File.join(@fixtures_path, 'CMSAWA_v5_6_Artifacts_missing_composite_files.zip')
-      value_set_loader = Measures::VSACValueSetLoader.new(options: @vsac_options, ticket_granting_ticket: get_ticket_granting_ticket_using_env_vars)
+      value_set_loader = Measures::VSACValueSetLoader.new(options: @vsac_options, vsac_api_key: test_api_key)
       loader = Measures::CqlLoader.new(measure_file, @measure_details, value_set_loader)
       assert_raises Measures::MeasureLoadingInvalidPackageException do
         loader.extract_measures
@@ -48,7 +48,7 @@ class CQLLoaderTest < Minitest::Test
   def test_loading_composite_measure
     VCR.use_cassette('measure__load_composite_measure', @vcr_options) do
       measure_file = File.new File.join(@fixtures_path, 'CMSAWA_v5_6_Artifacts.zip')
-      value_set_loader = Measures::VSACValueSetLoader.new(options: @vsac_options, ticket_granting_ticket: get_ticket_granting_ticket_using_env_vars)
+      value_set_loader = Measures::VSACValueSetLoader.new(options: @vsac_options, vsac_api_key: test_api_key)
       loader = Measures::CqlLoader.new(measure_file, @measure_details, value_set_loader)
       measures = loader.extract_measures
       assert_equal 8, measures.length
