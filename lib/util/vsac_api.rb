358658c5d88406c3d2da4a0d756cd5a1977a2945
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2023-09-08 16:14:28 -0400

    Replace api key validation by TGT generation with retrieval of a small VS (ONC Admin Sex). This verifies the UMLS API Key works with VSAC specifically and that VSAC is functional

--- lib/util/vsac_api.rb
+++ lib/util/vsac_api.rb
@@ -84,6 +84,16 @@ module Util
           raise VSACArgumentError.new("Required param :config is missing required URLs.")
         end
         @api_key = options[:api_key]
+        validate_api_key_vsac unless options[:api_key].nil?
+      end
+
+      ##
+      # Attempt to retrieve the ONC Admin Sex VS to verify
+      # VSAC connectivity with the supplied UMLS API Key.
+      def validate_api_key_vsac
+        response = get_multiple_valueset_raw_responses([{value_set: {oid: "2.16.840.1.113762.1.4.1"}, vs_vsac_options: {}}])[0]
+        raise VSACInvalidCredentialsError.new if !response.body || response.response_code == 401
+        validate_http_status(response.response_code)
       end
 
       ##
